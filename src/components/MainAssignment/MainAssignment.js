1e2fabd82c972412fe63b77183dd2250a2fa0fd1
vana <vanavanch09@gmail.com>
2022-02-16 12:03:48 +0530

    main assignment meks

--- src/components/MainAssignment/MainAssignment.js
+++ src/components/MainAssignment/MainAssignment.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import '../MainAssignment/style.css';
-
-
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import AceEditor from 'react-ace';
 import brace from 'brace';
 
@@ -19,30 +19,99 @@ import 'brace/theme/solarized_light';
 import 'brace/theme/cobalt';
 
 
+axios.defaults.baseURL = 'http://localhost:8000/';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Accept'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+
+
 const MainAssignment = () => {
+    let {id} = useParams();
+    console.log(id);
     const [code,setCode] = useState('');
     const [lang,setLang] = useState('python');
     const [theme,setTheme] = useState('cobalt');
     const [curlang,setCurlang] = useState('python');
 
+    //Assignment useState
+
+    const [title,setTitle] = useState('');
+    const [exin,setExin] = useState('');
+    const [exout,setExout] = useState('');
+    const [createdAt,setCreatedAt] = useState('');
+    const [body,setBody] = useState('');
+
     const getCode = () => {
-        console.log(code);
-        const codeOutput = document.getElementById('subout').value = code;
+      console.log(curlang);
+   
+      const codes = code;
+      const language = curlang;
+      const bodies = {
+        codes,
+        language
+      };
+      axios.get('/sanctum/csrf-cookie').then(response => {
+        axios.post('/api/practice', bodies).then(res=>{
+       
+          const codeOutput = res.data[0];
+          console.log(codeOutput);
+          document.getElementById('subout').value = codeOutput;
+        })
+      })
+       
     }
+
+    const submitCode = () => {
+      console.log(code);
+      console.log(lang);
+      console.log(curlang);
+      const zecode = code;
+      const language = curlang; 
+      const theDatas = {
+        zecode,
+        language
+      }
+      axios.get('sanctum/csrf-cookie').then(response => {
+        axios.post('/api/submit/'+id, theDatas).then(res =>{
+          console.log(res.data);
+        })
+      })
+
+    }
+
+    const getAssignment = () =>{
+        axios.get('/sanctum/csrf-cookie').then(response =>{
+          axios.get('/api/oneassignment/'+id).then(res => {
+            setTitle(res.data.theassignment.title);
+            setExin(res.data.theassignment.in);
+            setExout(res.data.theassignment.out);
+            setCreatedAt(res.data.theassignment.created_at);
+            setBody(JSON.stringify(res.data.theassignment.description));
+            // console.log(title);
+            // console.log(exin);
+            // console.log(exout);
+            // console.log(body);
+          })
+        })
+    }
+    useEffect(()=>{
+      getAssignment();
+    },[])
 
   return    <section className='assbody'>
   <div className='mainass-container'>
       <div className="ques">
-          <h3>Problem</h3>
+          <h3>{title}</h3>
           <div className='prop-con'>
           <div className="question">
               <p>
-                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id explicabo earum laudantium quas nisi inventore magnam eveniet quae harum. Totam dignissimos ducimus enim ipsam, modi mollitia. Voluptatibus aperiam voluptate quasi.
+                 {body}
               </p>
           </div>
           <div className="outin">
-              <p>Example Input:</p>
-              <p>Expected output: </p>
+              <p>Example Input: {exin}</p>
+              <p>Expected output: {exout} </p>
           </div>
           </div>
       </div>
@@ -110,7 +179,7 @@ const MainAssignment = () => {
  
   <div className='sub-container'>
   <button className='prac-run' onClick={getCode}>Run</button>
-  <button className='ass-submit'>Submit</button>
+  <button className='ass-submit' type='button' onClick={submitCode}>Submit</button>
   </div>
                     
       </div>
